8fdf57ec54ce7593fe30f5b6002629415cea7c8c
sarpedon <sarpedon.dev@gmail.com>
2026-03-10 14:42:24 +0100

    accidnetlally removed the wrong one

--- nix-dokploy.nix
+++ nix-dokploy.nix
@@ -484,7 +484,7 @@ in {
                   -v /var/run/docker.sock:/var/run/docker.sock \
                   -v ${cfg.dataDir}/traefik/traefik.yml:/etc/traefik/traefik.yml \
                   -v ${cfg.dataDir}/traefik/dynamic:/etc/dokploy/traefik/dynamic \
-                  -p 443:443/udp \
+                  -p 443:443/tcp \
                   ${lib.concatMapStringsSep " \\\n  " lib.escapeShellArg (cfg.traefik.extraArgs ++ [cfg.traefik.image])}
               fi
             '';
